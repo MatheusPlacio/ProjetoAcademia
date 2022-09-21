383e67df349edb6ab8c6518f87fd3e623b022b75
narba <narba@DESKTOP-KRRRREC>
2022-09-21 03:23:58 -0300

    Add project files.

---
+++ ProjetoAcademia.Infra.Data/Context/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoAcademia.Infra.Data.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+                
+        }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            base.OnModelCreating(mb);
+        }
+    }
+}
